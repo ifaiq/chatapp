bb45c6ef372439eb9ddd42d91ee4dc5fd195c423
Faiq Shariff <faiq55@yahoo.com>
2018-12-19 14:09:34 +0500

    heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const express = require('express');
 const publicpath = path.join(__dirname,'../public');
+var port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(publicpath));
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`server is on port${port}`);
+});
