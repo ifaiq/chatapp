4a946c676584d146e9384138f6676960fae6fddc
Faiq Shariff <faiq55@yahoo.com>
2019-01-02 12:56:55 +0500

    chat

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 const path = require('path');
 const express = require('express');
+const socket = require('socket.io');
+var http = require('http');
 const publicpath = path.join(__dirname,'../public');
 var port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 app.use(express.static(publicpath));
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New Connection');
+
+
+    socket.on('createMessage', (message)=>{
+    console.log('new message', message);
+    io.emit('newMessage',{
+        'from': message.from,
+        'text': message.text,
+        'createdAt': new Date().getTime()
+    });
+});
+socket.on('disconnect',()=>{
+        console.log('Disconnect');
+    })
+})
+
+server.listen(port,()=>{
     console.log(`server is on port${port}`);
 });
